11bb6c136f157a0d3682e15b28975bbdac37fd10
Robin Panta <hacktivistic@gmail.com>
2017-02-26 01:16:55 +0545

    Axe app

--- src/AxE/AxE.php
+++ src/AxE/AxE.php
@@ -28,11 +28,11 @@ class AxE extends Container implements IAxE
 
 
     /**
-     * Indicates if bootstrapped
+     * Indicates if executed
      *
      * @var bool
      */
-    protected $hasBeenBootstrapped = false;
+    protected $executed = false;
 
 
     /**
@@ -97,6 +97,78 @@ class AxE extends Container implements IAxE
      * @var string
      */
     protected $namespace;
+
+
+    /**
+     * Create a new AxE application
+     *
+     * @param  string|null  $basePath
+     * @return void
+     */
+    public function __construct($basePath = null)
+    {
+        $this->attendance();
+        $this->attendManagers();
+        $this->registerCoreContainerAliases();
+        if ($basePath) {
+            $this->setBasePath($basePath);
+        }
+    }
+
+
+    /**
+     * Register the basic bindings into the container.
+     *
+     * @return void
+     */
+    protected function attendance()
+    {
+        $this->instance('axe', $this);
+        $this->instance(Blade\Container\Container::class, $this);
+
+        static::setInstance($this);
+    }
+
+
+    /**
+     * Register all of the base service providers.
+     *
+     * @return void
+     */
+    protected function attendManagers()
+    {
+        $this->register(new EventManager($this));
+        $this->register(new RouteManager($this));
+    }
+
+    /**
+     * Determine if the managers have been executed
+     *
+     * @return bool
+     */
+    public function executed()
+    {
+        return $this->executed;
+    }
+
+
+    /**
+     * Run the given managers
+     *
+     * @param  array  $managers
+     * @return void
+     */
+    public function launch(array $managers)
+    {
+        $this->executed = true;
+        foreach ($managers as $manager) {
+
+            $this->handle($manager."_executing", [$this]);
+            $this->resolve($manager)->run($this);
+            $this->handle($manager."_executed", [$this]);
+
+        }
+    }
 
 
 }
