b862c78ca2a74c43c399a784c8b44968a8d35749
Robin Panta <hacktivistic@gmail.com>
2017-02-27 00:51:29 +0530

    Routing

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -362,7 +362,7 @@ class Router implements IRouter
         	$this->disableFetch($route);
         }else{
         	$route = $this->fetch($request);
-            $route->setMethod($request->method);
+            $route->setMethod($request->method());
         }
 
         $this->current = $route;
