697e6b6ad9416c8c8c9489f4d48019e6f2015011
Robin Panta <hacktivistic@gmail.com>
2017-03-06 03:08:55 +0530

    Edit

--- src/AxE/AxE_Error.php
+++ src/AxE/AxE_Error.php
@@ -10,17 +10,13 @@ class AxE_Error extends Exception implements Throwable
 	
 	function render($request, $ex)
 	{
-		var_dump($ex);
-	}
 
-/*
-$error_type = (!isset($error_type) or empty($error_type)) ? "404" : $error_type ;
-$error_title = (!isset($error_title) or empty($error_title)) ? "404 : Page not found!" : $error_title ;
-$error_msg = (!isset($error_msg) or empty($error_msg)) ? "Sorry, but the page was not found. You can try going back or keep staring at this page." : $error_msg ;
 
-$error_extra = (!isset($error_extra) or empty($error_extra)) ? "" : $error_extra ;
+$error_type = $ex->code;
+$error_title = $ex->message;
+$error_msg  = $ex->file. " - [ line ".$ex->line."]";
 
-$base_url = getenv('REQUEST_SCHEME')."://".getenv('HTTP_HOST');
+$base_url = $request->url();
 
 protected $output = <<<PHP
 
@@ -38,11 +34,10 @@ protected $output = <<<PHP
   		<h1><?=$error_title?></h1>
   		<h3><?=$error_msg?></h3>
 
-      <?=$error_extra?>
   </body>
 </html>
 
 PHP
 	;
-	*/
+	}
 }
