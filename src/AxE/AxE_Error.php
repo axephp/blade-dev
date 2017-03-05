ee6a86792e6fa4f95e1d3aadfe8b28ab3152dce1
Robin Panta <hacktivistic@gmail.com>
2017-03-06 03:14:30 +0530

    Edit

--- src/AxE/AxE_Error.php
+++ src/AxE/AxE_Error.php
@@ -25,13 +25,37 @@ $output = <<<PHP
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <base href="<?=$base_url?>" />
+    <base href="$base_url" />
     	<title>Error Occured | <?=SITE_NAME?></title>
-    	<link rel="stylesheet" type="text/css" href="css/error.css">
+    	<style type="text/css">
+    	*{
+		padding: 0px;
+		margin: 0px;
+		text-align: center;
+	}
+
+	body{
+		background-color: #ECF0F5;
+	}
+
+	h1{
+		font-family: Consolas;
+		margin-top: 100px;
+		margin-bottom: 20px;
+		font-size: 52px;
+		color: #ff3300;
+	}
+
+	h3{
+		font-family: Tahoma;
+		color: #0099ff;
+		margin-bottom: 50px;
+	}
+    	</style>
   </head>
   <body>
-  		<h1><?=$error_title?></h1>
-  		<h3><?=$error_msg?></h3>
+  		<h1>$error_title</h1>
+  		<h3>$error_msg</h3>
 
   </body>
 </html>
