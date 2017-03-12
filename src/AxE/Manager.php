97d33f8d2f020df686f01cb65c7aaf218620d2b4
Robin Panta <hacktivistic@gmail.com>
2017-03-12 15:39:19 +0530

    Managers

--- src/AxE/Manager.php
+++ src/AxE/Manager.php
@@ -4,7 +4,7 @@ namespace Blade\AxE;
 
 use ReflectionClass;
 use Exception;
-use Blade\Interfaces\AxE\IAxE;
+use Blade\Interfaces\AxE\IAxE as AxE;
 use Blade\Interfaces\AxE\IManager;
 use Blade\Container\Container;
 
