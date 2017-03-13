5a77dc0c68c13e89f1d3303184197f7f51bb75f7
Robin Panta <hacktivistic@gmail.com>
2017-03-13 20:55:56 +0530

    Error

--- src/AxE/AxE_Error.php
+++ src/AxE/AxE_Error.php
@@ -38,7 +38,7 @@ class AxE_Error extends Exception implements Throwable
 		}*/
 		
 		ob_start();
-		static::highlight_file_with_line_numbers($ex->getFile(), $error_line - 5, $error_line + 4);
+		static::highlight_file_with_line_numbers($ex->getFile(), $error_line - 8, $error_line + 8);
 		$coded = ob_get_contents();
 		ob_end_clean();
 
