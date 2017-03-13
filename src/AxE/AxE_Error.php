60273a3d776e09a9aa37f287914b9d6b66239d9f
Robin Panta <hacktivistic@gmail.com>
2017-03-13 20:10:17 +0530

    Error

--- src/AxE/AxE_Error.php
+++ src/AxE/AxE_Error.php
@@ -302,4 +302,32 @@ EOT;
 
 		return $response;
 	}
+
+	 public static function highlight_file_with_line_numbers($file, $from, $to) {
+          //Strip code and first span
+        $code = substr(highlight_file($file, true), 36, -15);
+        //Split lines
+        $lines = explode('<br />', $code);
+        //Count
+        $lineCount = count($lines);
+        //Calc pad length
+        $padLength = strlen($lineCount);
+       
+        //Re-Print the code and span again
+        echo "<code><span style=\"color: #000000\">";
+       
+        //Loop lines
+        for ($i=$from; $i <= $to ; $i++) { 
+        	 //Create line number
+        	$line = $lines[$i];
+            $lineNumber = str_pad($i + 1,  $padLength, '0', STR_PAD_LEFT);
+            //Print line
+            echo sprintf('<br><span style="color: #999999">%s | </span>%s', $lineNumber, $line);
+
+        }
+       
+        //Close span
+        echo "</span></code>";
+    }
+
 }
