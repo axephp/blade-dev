46e88541469f4711c9a0b94cfdc73b6c13be6bec
Robin Panta <hacktivistic@gmail.com>
2017-03-14 04:42:07 +0530

    Templating methods

---
+++ src/Routing/AssetRoute.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Blade\Routing;
+
+use Exception;
+
+use Blade\Interfaces\Routing\Router as IRouter;
+use Blade\Routing\Processor\Path;
+use Blade\Interfaces\Routing\CompiledRoute as ICompiledRoute;
+
+class AssetRoute extends Route
+{
+	
+}
