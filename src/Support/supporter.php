1f2b3309636a2a273288f9e2f97996d8a9f2d359
Robin Panta <hacktivistic@gmail.com>
2017-04-12 00:15:14 +0530

    routing

--- src/Support/supporter.php
+++ src/Support/supporter.php
@@ -68,7 +68,7 @@ if (!function_exists('redirect')) {
 
 	function redirect($url, $statusCode = 303)
 	{
-	   header('Location: ' . $url, true, $statusCode);
+	   header('Location: /' . $url, true, $statusCode);
 	   die();
 	}
 
