1b9d1e2b5f073f29d38e594a6d0f05c5376b17a7
Robin Panta <hacktivistic@gmail.com>
2017-04-12 23:23:41 +0530

    home page routing

--- src/Routing/Processor/Processor.php
+++ src/Routing/Processor/Processor.php
@@ -305,9 +305,8 @@ class Processor implements IProcessor
 					}elseif ($key == 0 && $param->getName() !== "args") {
 						// $args[] = $values[0] ?? null; // PHP 7.0
 						$args[] = isset($values['requests'][0]) ? $values['requests'][0] : null;
-					elseif ($key == 0 && count($params) == 1 && $param->getName() == "args") {
+					}elseif ($key == 0 && count($params) == 1 && $param->getName() == "args") {
 						$args = $values;
-					}
 					}else{
 						//$args[] = $values[$i] ?? null; // PHP 7.0
 						$args[] = isset($values['requests'][$i[$type]]) ? $values['requests'][$i['args']] : null;
