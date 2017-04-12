496b6c2dd88b56ead46fbf8e70d8d931dae65bdb
Robin Panta <hacktivistic@gmail.com>
2017-04-13 01:07:23 +0530

    routing params fix

--- src/Routing/Processor/Processor.php
+++ src/Routing/Processor/Processor.php
@@ -106,7 +106,7 @@ class Processor implements IProcessor
 
 		if (file_exists($file)) {
 
-			return $this->makeCompiledRoute($file, $request);
+			return $this->makeCompiledRoute($file, $request, $class);
 
 		}elseif (is_dir($dir) && !is_file($file)) {
 			
@@ -313,7 +313,7 @@ class Processor implements IProcessor
 
 
 
-	protected function makeCompiledRoute($file, $request)
+	protected function makeCompiledRoute($file, $request, $class = '')
 	{
 			include_once $file;
 			$class .= "\\".(implode("\\", array_flatten($request)));
