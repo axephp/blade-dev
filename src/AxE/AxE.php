d42854944876a43f6bcf321d7431344b22e68405
Jeevan Prakash Pant <jeevanppant@gmail.com>
2017-04-23 23:27:37 +0530

    validator

--- src/AxE/AxE.php
+++ src/AxE/AxE.php
@@ -117,7 +117,7 @@ class AxE extends Container implements IAxE
     protected function attendance()
     {
 
-        $this->map('axe', &$this);
+        $this->map('axe', $this);
         $this->map(\Blade\Container\Container::class, $this);
 
         $this->register(['events' => \Blade\Events\Trigger::class]);
