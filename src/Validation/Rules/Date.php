ccf93b01a3fca1b0f70dc597011ce578ecb52615
Jeevan Prakash Pant <jeevanppant@gmail.com>
2017-04-24 16:03:23 +0530

    validator

--- src/Validation/Rules/Date.php
+++ src/Validation/Rules/Date.php
@@ -12,8 +12,6 @@ class Date
 
 	public function execute(ValidationBuilder $validator)
 	{
-		$regexp = "^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$";
-
 		// Required
 		if ($this->required($validator)) {
 			return $this->required($validator);
@@ -28,7 +26,8 @@ class Date
 
 		}
 
-		$validDate = preg_match($regexp, $validDate);
+		$validDate = strtotime($validDate);
+		$validDate = checkdate(date("m, d, Y", $validDate));
 
 		if (!$validDate && $validator->value != '') {
 			return [
