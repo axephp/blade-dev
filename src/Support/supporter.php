e9e6d844d0d965c14c81055971f2963bd509e903
Jeevan Prakash Pant <jeevanppant@gmail.com>
2017-04-24 19:35:08 +0530

    validator

--- src/Support/supporter.php
+++ src/Support/supporter.php
@@ -116,8 +116,8 @@ if (!function_exists('match_date')) {
 					$checks[substr($value, 0, 1)] = ($catches[$key+1][0]);				
 				}
 				$greg = checkdate($checks['M'], $checks['D'], $checks['Y']);
-				dump($ret, $greg, $checks);
-				return ($greg);
+				
+				return ($ret && $greg);
 			}
 		}
 	}
