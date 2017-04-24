d9e79380d9fa4e27f6ee64956fb008771291f62d
Jeevan Prakash Pant <jeevanppant@gmail.com>
2017-04-24 12:10:36 +0530

    validator

---
+++ src/Validation/Rules/AlphaNumeric.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Blade\Validation\Rules;
+
+use Exception;
+use Blade\Validation\ValidationBuilder;
+use Blade\Validation\Rules\CommonRules;
+
+class AlphaNumeric
+{
+	use CommonRules;
+
+	public function execute(ValidationBuilder $validator)
+	{
+		// Required
+		if($this->required($validator)){
+			return $this->required($validator);
+		}
+
+		// START DATA TYPES
+		$regex = "/^(?=.*?[1-9])[0-9()-]/";
+
+		// ARGS
+		// none
+
+		$characters = filter($validator->value, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>$regex)));
+		if (!$characters && $validator->value != '') {
+			return [
+					"status"	=> "error",
+					"type"		=> "not-valid",
+					"message"	=> "The entered value is not valid."
+					];
+		}
+
+		// END DATA TYPES
+
+		// Common
+
+	}
+}
